6910ae2f0b997436d51a3e210c6018dc2cf06790
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-10-02 12:20:30 +0530

    updated initial route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,15 @@ import { TripsProvider } from "./context/TripsContext";
 export default function RootLayout() {
   return (
     <TripsProvider>
-      <Stack screenOptions={{ headerShown: false }} initialRouteName="screens/Welcome">
-        <Stack.Screen name="/screens/Welcome" />
+      <Stack
+        screenOptions={{ headerShown: false }}
+        initialRouteName="screens/Welcome"
+      >
+        <Stack.Screen name="screens/Welcome" />
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="screens/NewTrip" />
         <Stack.Screen name="screens/AddExpense" />
+        <Stack.Screen name="screens/ParticipantSummary" />
       </Stack>
     </TripsProvider>
   );
