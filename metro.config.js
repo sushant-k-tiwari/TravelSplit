95181b2bf81e726a5d6d8b62822fd6dfc8cdc441
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-10-07 23:05:34 +0530

    configured metro

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// metro.config.js
+const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
 
+// Get Expo's default Metro config
+const config = getDefaultConfig(projectRoot);
+
+// Pass the config through NativeWind
 module.exports = withNativeWind(config, { input: "./global.css" });
